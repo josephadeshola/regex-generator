1c75ffc73558f5939bc5e03e7779625bd88174fd
suri <swigder@gmail.com>
2017-09-22 22:51:18 +0200

    add support for letters and escaped characters (only . for now).  allow sending string instead of list of atoms.

--- regex.pl
+++ regex.pl
@@ -1,20 +1,96 @@
-digit(0).
-digit(1).
-digit(2).
-digit(3).
-digit(4).
-digit(5).
-digit(6).
-digit(7).
-digit(8).
-digit(9).
+% Strings should be interpreted as a list of character atoms.
+:- set_prolog_flag('double_quotes', 'chars').
+
+% Define the kinds of characters that exist.
+
+digit('0').
+digit('1').
+digit('2').
+digit('3').
+digit('4').
+digit('5').
+digit('6').
+digit('7').
+digit('8').
+digit('9').
+
+lower('a').
+lower('b').
+lower('c').
+lower('d').
+lower('e').
+lower('f').
+lower('g').
+lower('h').
+lower('i').
+lower('j').
+lower('k').
+lower('l').
+lower('m').
+lower('n').
+lower('o').
+lower('p').
+lower('q').
+lower('r').
+lower('s').
+lower('t').
+lower('u').
+lower('v').
+lower('w').
+lower('x').
+lower('y').
+lower('z').
+
+upper('A').
+upper('B').
+upper('C').
+upper('D').
+upper('E').
+upper('F').
+upper('G').
+upper('H').
+upper('I').
+upper('J').
+upper('K').
+upper('L').
+upper('M').
+upper('N').
+upper('O').
+upper('P').
+upper('Q').
+upper('R').
+upper('S').
+upper('T').
+upper('U').
+upper('V').
+upper('W').
+upper('X').
+upper('Y').
+upper('Z').
+
+letter(C) :- lower(C).
+letter(C) :- upper(C).
+
+unescaped(C) :- letter(C).
+unescaped(C) :- digit(C).
+
+escaped('.').
+escaped(C, E) :- escaped(C), atom_concat(\, C, E).
+
 
 % Start case and base case.
+
 regx(Cs, Rs) :- regx_i(Cs, Rs).
 regx_i([], []).
 
-% A character is itself in a regex.  Need to add exceptions for escaped characters.
-regx_i([C|Cs], [C|Rs]) :- regx_i(Cs, Rs).
+
+% Regular expressions.
+
+% Recognize unescaped characters as themselves.
+regx_i([C|Cs], [C|Rs]) :- unescaped(C), regx_i(Cs, Rs).
+
+% Recognize escaped characters as themselves if they should be escaped.
+regx_i([C|Cs], [E|Rs]) :- escaped(C, E), regx_i(Cs, Rs).
 
 % Recognize digits.
 regx_i([C|Cs], [\d|Rs]) :- digit(C), regx_i(Cs, Rs).
--- regex_test.pl
+++ regex_test.pl
@@ -2,11 +2,19 @@
 
 :- begin_tests(regx).
 
+:- set_prolog_flag('double_quotes', 'chars').
+
 test(digit) :-
-    findall(R, regx([1], R), Rs),
+    findall(R, regx("1", R), Rs),
         Rs == [
-            [1],
+            ['1'],
             [\d]
+        ].
+
+test(escaped) :-
+    findall(R, regx(['.'], R), Rs),
+        Rs == [
+            ['\\.']
         ].
 
 :- end_tests(regx).
