511e84436edbbee0bf7d550ab05bc825d5cc561b
suri <swigder@gmail.com>
2017-10-03 14:17:16 +0200

    using intermediary structures to represent regexs.  results are now atoms rather than lists.

--- regex.pl
+++ regex.pl
@@ -9,8 +9,7 @@
 %   ?Rs - list of elements in a regular expression
 % If Rs is bound, checks if the regular expression represented by Rs matches Cs.
 % If Rs is not bound, returns (as Rs) all implemented regular expressions that match Cs. No duplicates will be returned.
-regex(Cs, [R|Rs]) :- regex([], Cs, [R|Rs]).
-regex([], [], []).
+regex(Cs, R) :- regex_tokens([], Cs, Rs), regex_encode(Rs, R).
 
 % procedure regex_multi(+Ss, ?Rs):
 %   +Ss - list of strings or list of lists of characters
@@ -20,7 +19,7 @@ regex([], [], []).
 %   will be returned.
 % regex_multi([S1, S2, ..., Sn], Rs) is equivalent to regex(S1, Rs), regex(S2, Rs), ..., regex(Sn, Rs).
 regex_multi([S|Ss], R) :- regex(S, R), regex_multi(Ss, R).
-regex_multi([], R).
+regex_multi([], _).
 
 % procedure regex_multi(+Ss, +Xs, ?Rs):
 %   +Ss - list of strings or list of lists of characters
@@ -33,24 +32,37 @@ regex_multi([], R).
 %   regex(Sn, Rs), \+ regex(X1, Rs), \+ regex(X2, Rs), ..., \+ regex(Xn, Rs).
 regex_multi([S|Ss], Xs, R) :- regex(S, R), regex_multi(Ss, Xs, R).
 regex_multi([], [X|Xs], R) :- \+regex(X, R), regex_multi([], Xs, R).
-regex_multi([], [], R).
+regex_multi([], [], _).
+
+
+% Convert regular expression tokens to regular expression strings.
+regex_encode([R|Rs], S) :- regex_encode(R, Sr), regex_encode(Rs, Ss), atom_concat(Sr, Ss, S).
+regex_encode([], '').
+regex_encode(class(C), C).
+regex_encode(plus(E), P) :- regex_encode(E, C), atom_concat(C, +, P).
 
 
 % Regular expression rules.
 
 % Recognize unescaped characters as themselves.
-regex(Cs, [C|Ps], [C|Rs]) :- unescaped(C), regex(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class(C)|Rs]) :- unescaped(C), regex_tokens(Cs, Ps, Rs).
 
 % Recognize escaped characters as themselves if they should be escaped.
-regex(Cs, [C|Ps], [E|Rs]) :- escaped(C, E), regex(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class(E)|Rs]) :- escaped(C, E), regex_tokens(Cs, Ps, Rs).
 
 % Recognize character classes.
-regex(Cs, [C|Ps], ['\\d'|Rs]) :- digit(C), regex(Cs, Ps, Rs).
-regex(Cs, [C|Ps], ['[A-Z]'|Rs]) :- upper(C), regex(Cs, Ps, Rs).
-regex(Cs, [C|Ps], ['[a-z]'|Rs]) :- lower(C), regex(Cs, Ps, Rs).
-regex(Cs, [C|Ps], ['[a-zA-Z]'|Rs]) :- letter(C), regex(Cs, Ps, Rs).
-regex(Cs, [C|Ps], ['\\w'|Rs]) :- word(C), regex(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class('\\d')|Rs]) :- digit(C), regex_tokens(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class('[A-Z]')|Rs]) :- upper(C), regex_tokens(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class('[a-z]')|Rs]) :- lower(C), regex_tokens(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class('[a-zA-Z]')|Rs]) :- letter(C), regex_tokens(Cs, Ps, Rs).
+regex_tokens(Cs, [C|Ps], [class('\\w')|Rs]) :- word(C), regex_tokens(Cs, Ps, Rs).
 
 % Recognize repetition.
-regex([], [C], [R,+]) :- regex([], [C], [R]).
-regex(Cs, [C|Ps], [R,+|Rs]) :- regex([], [C], [R]), regex(Cs, Ps, [R,+|Rs]).
+regex_tokens([], [C], [plus(R)]) :- iterable(R), regex_tokens([], [C], [R]).
+regex_tokens(Cs, [C|Ps], [plus(R)|Rs]) :- iterable(R), regex_tokens([], [C], [R]), regex_tokens(Cs, Ps, [plus(R)|Rs]).
+
+% Base case.
+regex_tokens([], [], []).
+
+% Allow iteration only on certain types of regex tokens.
+iterable(class(_)).
--- regex_test.pl
+++ regex_test.pl
@@ -4,47 +4,53 @@
 
 :- begin_tests(regex).
 
-single_char_regex(S, R) :- regex(S, R), length(R, 1).
-
 test(digit) :-
-    findall(R, single_char_regex("1", R), Rs),
+    findall(R, regex("1", R), Rs),
         Rs == [
-            ['1'],
-            ['\\d'],
-            ['\\w']
+            '1',
+            '\\d',
+            '\\w',
+            '1+',
+            '\\d+',
+            '\\w+'
         ].
 
 test(escaped) :-
-    findall(R, single_char_regex(".", R), Rs),
+    findall(R, regex(".", R), Rs),
         Rs == [
-            ['\\.']
+            '\\.',
+            '\\.+'
         ].
 
 test(lower) :-
-    findall(R, single_char_regex("a", R), Rs),
+    findall(R, regex("a", R), Rs),
         Rs == [
-            ['a'],
-            ['[a-z]'],
-            ['[a-zA-Z]'],
-            ['\\w']
+            'a',
+            '[a-z]',
+            '[a-zA-Z]',
+            '\\w',
+            'a+',
+            '[a-z]+',
+            '[a-zA-Z]+',
+            '\\w+'
         ].
 
 test(repetition) :-
     findall(R, regex_multi(["a", "aa", "aaa"], R), Rs),
         Rs == [
-            ['a',+],
-            ['[a-z]',+],
-            ['[a-zA-Z]',+],
-            ['\\w',+]
+            'a+',
+            '[a-z]+',
+            '[a-zA-Z]+',
+            '\\w+'
         ].
 
 test(negation) :-
     findall(R, regex_multi(["12", "345"], ["6"], R), Rs),
         Rs == [
-            ['\\d','\\d',+],
-            ['\\d','\\w',+],
-            ['\\w','\\d',+],
-            ['\\w','\\w',+]
+            '\\d\\d+',
+            '\\d\\w+',
+            '\\w\\d+',
+            '\\w\\w+'
         ].
 
 :- end_tests(regex).
